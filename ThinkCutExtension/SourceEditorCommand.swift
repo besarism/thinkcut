9b94f8af2e75cd45fa907cadf48702bccd2caba6
besarism <b3sar11@gmail.com>
2025-04-08 08:33:51 +0200

    first commit

---
+++ ThinkCutExtension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  ThinkCutExtension
+//
+//  Created by Besar Ismaili on 08/04/2025.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
