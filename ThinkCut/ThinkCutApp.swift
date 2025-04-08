1f75e566277ab31f17e5ef8cca18071e9d1bfd8d
besarism <b3sar11@gmail.com>
2025-04-08 08:31:58 +0200

    Initial Commit

---
+++ ThinkCut/ThinkCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkCutApp.swift
+//  ThinkCut
+//
+//  Created by Besar Ismaili on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThinkCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThinkCutTests/ThinkCutTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkCutTests.swift
+//  ThinkCutTests
+//
+//  Created by Besar Ismaili on 08/04/2025.
+//
+
+import Testing
+@testable import ThinkCut
+
+struct ThinkCutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
